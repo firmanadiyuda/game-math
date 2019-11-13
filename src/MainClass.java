f8658b1f7018c345072ddb31ab9fe500d370e217
Firman Adiyuda <firmanadiyuda@gmail.com>
2019-11-13 19:00:28 +0700

    Update MainClass.java

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,9 +1,27 @@
+
 import backend.*;
-import frontend.*;
+import frontend.View;
+import java.util.Scanner;
 
 public class MainClass {
 
+    // Main class.
     public static void main(String[] args) {
+
+        // Buat objek baru dan instansiasi nilai awal.
+        Player player = new Player();
+        
+
+        // Masuk ke tampilan utama untuk input nama.
+        // Parameter pertama 'player' untuk passing objek 'player'.
+        View.tampilanUtama(player);
+
+
+        // Masuk ke menu Utama.
+        // Masuk ke tampilan Menu.
+        View.tampilanMenu(player);
+       
+
 
    }
 
