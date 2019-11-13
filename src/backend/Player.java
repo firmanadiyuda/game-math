644cbded374163cd443ac22959a957a46095cc40
Devin Michael Brahmasta <alexander.devin2000@gmail.com>
2019-11-13 18:46:41 +0700

    Update Player.java

--- src/backend/Player.java
+++ src/backend/Player.java
@@ -2,4 +2,41 @@ package backend;
 
 public class Player {
     
+    
+    // Attribut Player
+    private String nama;
+    private int skor;
+    private int nyawa;
+    
+    // Konstruktor Player
+    public Player() {
+        nama = "Anonim";
+        skor = 0;
+        nyawa = 3;
+    }
+    
+    // Method Setter dan Getter.
+    public void setNama(String inputNama){
+        nama = inputNama;
+    }
+    
+    public String getNama(){
+        return nama;
+    }
+
+    public int getSkor() {
+        return skor;
+    }
+
+    public void setSkor(int skor) {
+        this.skor = skor;
+    }
+
+    public int getNyawa() {
+        return nyawa;
+    }
+
+    public void setNyawa(int nyawa) {
+        this.nyawa = nyawa;
+    }
 }
